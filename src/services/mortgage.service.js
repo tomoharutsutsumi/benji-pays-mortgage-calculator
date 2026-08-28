d0f8925beaf92844907ef35f3c02375db0ed5eb7
tomoharutsutsumi <ttomoharu201@gmail.com>
2026-08-28 13:17:41 -0700

    add: draft initial mortgage payment formula

--- src/services/mortgage.service.js
+++ src/services/mortgage.service.js
@@ -7,12 +7,21 @@ const calculateMortgage = ({
 }) => {
     // Mortgage calculation logic will go here.
 
+    const baseMortgage = propertyPrice - downPayment
+    const downPaymentRatio = (downPayment / propertyPrice) * 100
+    const cmhcInsurance = baseMortgage * premiumRate(downPaymentRatio)
+    const principal = baseMortgage + cmhcInsurance
+    const periodicInterestRate = annualInterestRate / paymentsPerYear(paymentSchedule)
+    const numberOfPayments = amortizationYears * paymentsPerYear(paymentSchedule)
+    const mortgagePayment = 
+        principal * 
+        (
+            (periodicInterestRate * Math.pow(1 + periodicInterestRate, numberOfPayments)) / 
+            (Math.pow(1 + periodicInterestRate, numberOfPayments) - 1)
+        );
+
     return {
-        propertyPrice,
-        downPayment,
-        annualInterestRate,
-        amortizationYears,
-        paymentSchedule,
+        mortgagePayment
     };
 };
 
